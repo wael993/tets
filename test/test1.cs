7d337015d0780bf1d58bd9c86b0fda8262225c67
Linda Jost <jost@halbergsoft.de>
2022-01-16 17:22:21 +0100

    erst

---
+++ test/test1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+   public class artikel
+    {
+        public artikel(string obst_name, string obst_herkunft, int obst_preis)
+        {
+            this.obst_name = obst_name;
+            this.obst_herkunft = obst_herkunft;
+            this.obst_preis = obst_preis;
+        }
+
+        public string obst_name { get; set; }
+        public string obst_herkunft { get; set; }
+        public int obst_preis { get; set; }
+
+    }
+    public class Invoice
+    {
+        artikel banana = new artikel("banana","somal" ,1);
+
+
+    }
+
+}
